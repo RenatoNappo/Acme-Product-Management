fc69ad45446c77df3ff428a1b355e0b8fc0bacf5
RenatoNappo <31985727+RenatoNappo@users.noreply.github.com>
2018-01-12 10:07:11 +0000

    Input/Output properties added

---
+++ src/app/shared/star/star.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit, OnChanges {
+
+  starWidth: number;
+  @Input() rating: number;
+
+@Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(){
+      this.starWidth = (this.rating * (86 / 5));
+  }
+
+  onClick(): void{
+    this.ratingClicked.emit("The rating " +  this.rating + " was clicked.");
+  }
+
+}
