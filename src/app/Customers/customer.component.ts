c870db27ae51f6877792c7a0a517e892f910a316
RenatoNappo <31985727+RenatoNappo@users.noreply.github.com>
2018-02-17 07:16:33 +0000

    tslint changes (validation)

--- src/app/Customers/customer.component.ts
+++ src/app/Customers/customer.component.ts
@@ -5,8 +5,7 @@ import { AbstractControl } from '@angular/forms';
 
 
 function ratingRange(c: AbstractControl ): {[key: string]: boolean } | null {
-  if ( c.value != undefined && ( isNaN(c.value) || c.value < 1 || c.value > 5 ))
-  {
+  if ( c.value !== undefined && ( isNaN(c.value) || c.value < 1 || c.value > 5 ))  {
     return {'range': true};
   }
   return null;
