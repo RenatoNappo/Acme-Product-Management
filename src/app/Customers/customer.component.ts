2a7b6b232f528374fe23e438dfd56956981aa2b6
RenatoNappo <renatonappo@hotmail.com>
2018-02-21 06:52:19 +0000

    cross-field validation

--- src/app/Customers/customer.component.ts
+++ src/app/Customers/customer.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn} from '@angular/forms';
 import { Customer } from './customer';
 import { AbstractControl } from '@angular/forms';
 
 
-function ratingRange(c: AbstractControl ): {[key: string]: boolean } | null {
-  if ( c.value !== undefined && ( isNaN(c.value) || c.value < 1 || c.value > 5 ))  {
-    return {'range': true};
+function ratingRange(minValue: number, maxValue: number): ValidatorFn {
+  return (c: AbstractControl ): {[key: string]: boolean } | null => {
+    if ( c.value !== undefined && ( isNaN(c.value) || c.value < minValue || c.value > maxValue ))  {
+      return {'range': true};
+    }
+    return null;
+  }
+}
+
+
+function emailComparison(c: AbstractControl){
+  let email = c.get('email');
+  let confirmEmail = c.get('confirmEmail');
+  if(email.pristine || confirmEmail.pristine){
+    return null;
+  }
+  if(email.value !== confirmEmail.value) {
+    return {'emailMatch': true};
   }
   return null;
 }
@@ -29,11 +44,21 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', Validators.required],
+      emailGroup: this.fb.group({
+        email: ['', Validators.required],
+        confirmEmail: ['', Validators.required],
+      },{validator: emailComparison}),
       phone: '',
       notification: 'email',
-      rating: ['', ratingRange],
-      sendCatalog: true
+      rating: ['', ratingRange(1,5)],
+      sendCatalog: true,
+      catalogAddress: this.fb.group({
+        streeAddress1: '',
+        streetAddress2: '',
+        city: '',
+        state: '',
+        zipcode: ''
+      })
     });
   }
 
@@ -46,7 +71,11 @@ export class CustomerComponent implements OnInit {
   }
 
   get email(){
-    return this.customerForm.get('email');
+    return this.customerForm.get('emailGroup.email');
+  }
+
+  get confirmEmail(){
+    return this.customerForm.get('emailGroup.confirmEmail');
   }
 
   get phone(){
@@ -61,6 +90,26 @@ export class CustomerComponent implements OnInit {
     return this.customerForm.get('sendCatalog');
   }
 
+  get streeAddress1(){
+    return this.customerForm.get('catalogAddress.streeAddress1');
+  }
+
+  get streeAddress2(){
+    return this.customerForm.get('catalogAddress.streeAddress2');
+  }
+
+  get city(){
+    return this.customerForm.get('catalogAddress.city');
+  }
+
+  get state(){
+    return this.customerForm.get('catalogAddress.state');
+  }
+
+  get zipcode(){
+    return this.customerForm.get('catalogAddress.zipcode');
+  }
+ 
 
   setNotfication(notifyVia: string): void {
     if (notifyVia === 'phone' ) {
